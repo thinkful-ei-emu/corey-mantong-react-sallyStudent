a12dba01f7e9c805284366e4f952a81a43015101
Corey Moore <monorthwest@gmail.com>
2019-06-24 08:59:56 -0700

    sally student resume displays

---
+++ sally-student-resume/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>About me</h1>
+        <p>I'm an aspiring full stack web developer. I love cats, coding, and crocheting.</p>
+      </header>
+
+      <main> 
+        <h2 className="skills">Skills</h2>
+          <ul className="ul-skills">
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>jQuery</li>
+          </ul>
+        
+        <section>
+          <h2>Contact Info</h2>
+          <p>Twitter: <a href="http://twitter.com/sallystudent">@SallyStudent</a></p>
+      </section>
+      </main>
+   
+    </div>
+  );
+}
+
+export default App;
